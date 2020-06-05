8158c2ec10159a925f08a67fef6a70e38c357604
Roman <romabruzak@gmail.com>
2020-06-05 07:07:42 +0300

    added image array, functionality button, disabled button

--- slider.js
+++ slider.js
@@ -2,12 +2,30 @@ const buttonPrevious = document.getElementById('buttonPrevious');
 const buttonNext = document.getElementById('buttonNext');
 const sliderImage = document.querySelector('.slide');
 
+let currentIndex = 0;
+const image = [];
+
+image.push('https://klike.net/uploads/posts/2019-06/1560838551_1.jpg');
+
+sliderImage.src = image[currentIndex];
+buttonPrevious.disabled = true;
+
 function onClickPrevious() {
-    alert('Previous')
+    buttonNext.disabled = false;
+    currentIndex--;
+    sliderImage.src = image[currentIndex];
+    if (currentIndex === 0) {
+        buttonPrevious.disabled = true
+    }
 }
 
 function onClickNext() {
-alert('Next')
+    buttonPrevious.disabled = false;
+    currentIndex++;
+    sliderImage.src = image[currentIndex];
+    if (currentIndex === image.length - 1) {
+        buttonNext.disabled = true
+    }
 }
 
 buttonPrevious.addEventListener('click', onClickPrevious);
