93272951e0294d08025b8eb1b9dd82fed10f1e00
Roman <romabruzak@gmail.com>
2020-06-05 06:45:58 +0300

    slider

---
+++ slider.js
@@ -0,0 +1,14 @@
+const buttonPrevious = document.getElementById('buttonPrevious');
+const buttonNext = document.getElementById('buttonNext');
+const sliderImage = document.querySelector('.slide');
+
+function onClickPrevious() {
+    alert('Previous')
+}
+
+function onClickNext() {
+alert('Next')
+}
+
+buttonPrevious.addEventListener('click', onClickPrevious);
+buttonNext.addEventListener('click', onClickNext);
