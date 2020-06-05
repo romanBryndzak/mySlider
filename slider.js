9cd992915a8f1fff1184403256fc326c8cda9acc
Roman <romabruzak@gmail.com>
2020-06-05 20:02:37 +0300

    prototype

--- slider.js
+++ slider.js
@@ -6,41 +6,12 @@ function Slider() {
     this.currentIndex = 0;
     this.image = [];
 
-    this.startInitialization = function (elId) {
-
-        let element = document.getElementById(elId);
-        this.buttonPrevious = element.querySelector('.buttonPrevious');
-        this.buttonNext = element.querySelector('.buttonNext');
-        this.sliderImage = element.querySelector('.slide');
-
-        const sliderThis = this;
-        this.image.push('https://klike.net/uploads/posts/2019-06/1560838551_1.jpg');
-
-        this.buttonPrevious.addEventListener('click', function (e) {
-            if (elId === 'sliderDisabled') {
-                sliderThis.onClickPreviousDisabled(e)
-            } else if (elId === 'sliderInfinite') {
-                sliderThis.onClickPreviousInfinite(e)
-            }
-        });
-        this.buttonNext.addEventListener('click', function (e) {
-            if (elId === 'sliderDisabled') {
-                sliderThis.onClickNextDisabled(e)
-            } else if (elId === 'sliderInfinite') {
-                sliderThis.onClickNextInfinite(e)
-            }
-        });
-
-        this.sliderImage.src = this.image[this.currentIndex];
-        this.buttonPrevious.disabled = true;
-    };
-
     this.onClickPreviousDisabled = function () {
         this.buttonNext.disabled = false;
         this.currentIndex--;
         if (this.currentIndex === 0) {
             this.buttonPrevious.disabled = true
-        } else if (this.currentIndex === this.image.length - 2) {
+        } else if (this.currentIndex === - 1) {
             this.currentIndex = 0;
             this.buttonPrevious.disabled = true
         }
@@ -76,3 +47,35 @@ function Slider() {
         this.sliderImage.src = this.image[this.currentIndex];
     };
 }
+
+Slider.prototype.startInitialization = function (elId) {
+
+    let element = document.getElementById(elId);
+    this.buttonPrevious = element.querySelector('.buttonPrevious');
+    this.buttonNext = element.querySelector('.buttonNext');
+    this.sliderImage = element.querySelector('.slide');
+
+    const sliderThis = this;
+    this.image.push('https://klike.net/uploads/posts/2019-06/1560838551_1.jpg');
+    this.image.push('https://i.ytimg.com/vi/Qq6b3v_0AtM/maxresdefault.jpg');
+    this.image.push('https://i.ytimg.com/vi/NYTrF1yQ1u0/maxresdefault.jpg');
+    this.image.push('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSo5oACWf92GRRhy30CoSBy0B9i6qlc7vU8OwFBBvqBmq1g-O8u&usqp=CAU');
+
+    this.buttonPrevious.addEventListener('click', function (e) {
+        if (elId === 'sliderDisabled') {
+            sliderThis.onClickPreviousDisabled(e)
+        } else if (elId === 'sliderInfinite') {
+            sliderThis.onClickPreviousInfinite(e)
+        }
+    });
+    this.buttonNext.addEventListener('click', function (e) {
+        if (elId === 'sliderDisabled') {
+            sliderThis.onClickNextDisabled(e)
+        } else if (elId === 'sliderInfinite') {
+            sliderThis.onClickNextInfinite(e)
+        }
+    });
+
+    this.sliderImage.src = this.image[this.currentIndex];
+    this.buttonPrevious.disabled = true;
+};
